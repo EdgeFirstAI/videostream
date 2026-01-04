e62f24a90364f0a0e2a31cdeed5778e85982429e
Sébastien Taylor <sebastien@au-zone.com>
2026-01-04 11:50:10 -0700

    Version 2.1.0
    Signed-off-by: Sébastien Taylor <sebastien@au-zone.com>

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ copyright = '2025 Au-Zone Technologies'
 author = 'Au-Zone Technologies'
 
 # The short X.Y version
-version = '2.0.0'
+version = '2.1.0'
 # The full version, including alpha/beta/rc tags
 release = version
 
